43289683def06272001d1ba22628b55e54e35241
Jamile Santana <s.jamilesantana@gmail.com>
2021-10-29 22:37:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_word():
-    return "<p> Olá,<b>turma do função</b>!</p>" 
+    return "<p> Olá,<b>turma do fundão</b>!</p> Esse é meu primeiro site" 
