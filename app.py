135819719af84a2b8c5285a8c8d0cffe1abddf1c
Jamile Santana <s.jamilesantana@gmail.com>
2021-11-05 20:21:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_word():
     return "<p> Olá! <b> Esse é meu primeiro site</b>!</p>" 
+
+@app.route("/sobre")
+def sobre():
+    return "<h1>Sobre</h1> <p> Esse site foi criado por <b>Jamile Santana </b>.</p>"
