57ae5b39e7c646cd08f6af282b7d2b21c62dbb1a
Jamile Santana <s.jamilesantana@gmail.com>
2021-11-05 20:57:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 @app.route("/")
 def hello_word():
     return """<h1>Olá mundo!</h1> 
-    <p><b>Esse é meu primeiro site</b>!</p>"""
+    <p><b>Esse é meu primeiro site</b>!</p>
+    <a href="/sobre">Sobre esse site</a>
+    """
+
 
 @app.route("/sobre")
 def sobre():
     return """<h1>Sobre</h1> 
+    
+    <a href="/">Página inicial</a>
+    
     <p> Esse site foi criado por <b>Jamile Santana </b>.</p>"""
+
+
 
 
