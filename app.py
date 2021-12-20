836e73cd5453ac47285b766e1c4df9ddd3dcfb86
Jamile Santana <s.jamilesantana@gmail.com>
2021-12-20 19:08:25 -0300

    codigo novo

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-
 import csv
 import pandas as pd
 from GoogleNews import GoogleNews
 from flask import Flask, render_template 
-app = Flask(__name__)
 
 def noticias():
     googlenews = GoogleNews()
     googlenews.set_lang('pt-br')
     googlenews.get_news("Mogi das Cruzes")
     resultado = googlenews.result()
-    df =  pd.DataFrame(resultado)
-    return render_template("noticias.html", dados = df.to_html())
+    return pd.DataFrame(resultado)
 
-
+app = Flask(_name_)
 @app.route("/")
 def hello_word():
     return render_template("home.html")
@@ -27,6 +24,11 @@ def sobre():
 
 @app.route("/raspador_noticias")
 def raspador_noticias():
-    noticias_mogi = noticias()
-    return render_template("noticias.html", dados = df.to_html())
+    googlenews = GoogleNews()
+    googlenews.set_lang('pt-br')
+    googlenews.get_news("Mogi das Cruzes")
+    resultado = googlenews.result()
+    df = pd.DataFrame(resultado) #coloca o resultado em uma tabela  
+    return render_template("raspador_noticias.html", dados = df.to_html) # chama a variável dados que contém df para ser mostrada nesta seção do site
+
  
