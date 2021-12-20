ce2a5c0ce2a48331f6c725e8c216c89ca70bddbc
Jamile Santana <s.jamilesantana@gmail.com>
2021-12-19 23:47:06 -0300

    modifiquei função notícias

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ def noticias():
     googlenews.set_lang('pt-br')
     googlenews.get_news("Mogi das Cruzes")
     resultado = googlenews.result()
-    return pd.DataFrame(resultado)
-
-def path_to_image_html(path):
+    path_to_image_html(path):
   return '<img src= "' + path + '" width ="60" >'
     resultado.to_html(escape = False, formatters=dict(img=path_to_image_html ))
     from IPython.core.display import HTML
-    HTML (resultado.to_html(escape=False, formatters=dict(img=path_to_image_html )))
+  return HTML (resultado.to_html(escape=False, formatters=dict(img=path_to_image_html )))
+
+
 
 app = Flask(__name__)
 @app.route("/")
